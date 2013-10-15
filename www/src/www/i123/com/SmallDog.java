2963dfbd834bd0c80b0c9b6c4cc8fdd7b0045f3e
test_user1 <test_user1@qq.com>
2013-10-15 22:28:25 +0800

    2..

--- www/src/www/i123/com/SmallDog.java
+++ www/src/www/i123/com/SmallDog.java
@@ -2,4 +2,23 @@ package www.i123.com;
 
 public class SmallDog {
 
+    private Integer height;
+    private Integer age;
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
 }
