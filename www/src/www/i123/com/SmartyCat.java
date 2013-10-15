a0c2587cea4fad6645cf44400b6dd5c6ece8ca48
i123 <i123@qq.com>
2013-10-15 22:38:02 +0800

    My SmartyCat.java

---
+++ www/src/www/i123/com/SmartyCat.java
@@ -0,0 +1,14 @@
+package www.i123.com;
+
+public class SmartyCat {
+    private String name;
+    private String interestings;
+
+    public void getName() {
+        return this.name;
+    };
+
+    public void getInterestings() {
+        return this.interestings;
+    }
+}
